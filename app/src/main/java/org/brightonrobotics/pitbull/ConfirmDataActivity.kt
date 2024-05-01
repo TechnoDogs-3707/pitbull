f97608be46a3155a61fe317bb204d5734bb9eebe
Noah Harper <qwrty.streams@gmail.com>
2024-05-01 17:49:27 -0400

    code generation and stuff in db

--- app/src/main/java/org/brightonrobotics/pitbull/ConfirmDataActivity.kt
+++ app/src/main/java/org/brightonrobotics/pitbull/ConfirmDataActivity.kt
@@ -1,3 +1,7 @@
+// i am sorry for this nightmarish code
+// good luck
+// the generateUniqueJoinCode function is found in Utils.kt btw
+
 package org.brightonrobotics.pitbull
 
 import android.content.Intent
@@ -5,6 +9,11 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.io.File
 
 class ConfirmDataActivity : AppCompatActivity() {
@@ -13,6 +22,7 @@ class ConfirmDataActivity : AppCompatActivity() {
     private lateinit var yesButton: Button
     private lateinit var noButton: Button
 
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.confirm_data)
@@ -37,7 +47,17 @@ class ConfirmDataActivity : AppCompatActivity() {
                     }
                 """.trimIndent()
             )
-            startActivity(Intent(this, MainActivity::class.java))
+
+            GlobalScope.launch {
+                generateUniqueJoinCode(intent.getStringExtra("teamNumber")!!)
+            }
+
+            val storage = Firebase.storage("gs://pitbull-421814.appspot.com")
+            val usersDirectory = storage.reference.child("data").child(intent.getStringExtra("teamNumber").toString()).child("users")
+            val dummyFile = usersDirectory.child("dummy.txt")
+            dummyFile.putBytes("dummy".toByteArray())
+
+            startActivity(Intent(this, DisplayTeamDataActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/org/brightonrobotics/pitbull/Utils.kt
@@ -0,0 +1,37 @@
+package org.brightonrobotics.pitbull
+
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.StorageException
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.tasks.await
+import java.util.*
+
+suspend fun generateUniqueJoinCode(teamNumber: String) {
+    val storage = Firebase.storage("gs://pitbull-421814.appspot.com")
+    val directory = storage.reference.child("data").child(teamNumber)
+
+    var uniqueCode: String
+    do {
+        uniqueCode = "${System.currentTimeMillis()}${Random().nextInt(999)}"
+    } while (codeExistsInFirebase(directory, uniqueCode))
+
+    val codeFile = directory.child("code.txt")
+    codeFile.putBytes(uniqueCode.toByteArray()).await()
+}
+
+suspend fun codeExistsInFirebase(directory: StorageReference, code: String): Boolean {
+    val codeFile = directory.child("code.txt")
+    return try {
+        val existingCode = codeFile.getBytes(Long.MAX_VALUE).await().toString(Charsets.UTF_8)
+        existingCode == code
+    } catch (e: StorageException) {
+        if (e.httpResultCode == 404) {
+            // File does not exist, return false
+            false
+        } else {
+            // Other exceptions, rethrow them
+            throw e
+        }
+    }
+}
